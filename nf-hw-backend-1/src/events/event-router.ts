b8742cd67675723353c02c2096328680428032a8
Dalida27 <100573511+Dalida27@users.noreply.github.com>
2024-06-11 23:45:15 +0500

    Update event-router.ts

--- nf-hw-backend-1/src/events/event-router.ts
+++ nf-hw-backend-1/src/events/event-router.ts
@@ -10,10 +10,11 @@ const eventRouter = Router();
 const eventService = new EventService();
 const eventController = new EventController(eventService);
 
+eventRouter.get('/events/location', authMiddleware, eventController.getEventsByLocation);
+
 eventRouter.get('/events/', eventController.getEvents);
 eventRouter.post('/events/', eventController.createEvent);
 eventRouter.get('/events/:id', eventController.getEventById);
 
-eventRouter.get('/events/location', authMiddleware, eventController.getEventsByLocation);
 
 export default eventRouter;
